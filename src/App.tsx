ddfe8d557d84c7d776fbdf53af53909ffbd55e07
tasoskrv <tasos.karavanis@gmail.com>
2021-11-21 23:45:55 +0200

    css

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,11 @@ function App() {
   const loginUsecase = useInjection(LoginUsecase);
 
   return (
-    <div className="wrapper">
+    <div className="wrapper">      
       <Container className="h-100">
         <Row className="h-100">
           <Col className="first-col p-0">
+          <div className="cloudocean">cloud<span className="ocean">Ocean</span></div>
             <Router>
               <Routes>
                   <Route path="/" element={
@@ -31,9 +32,9 @@ function App() {
               </Routes>          
             </Router>            
           </Col>
-          <Col className="second-col p-0">
-            <img className="img-background" src="https://www.thoughtco.com/thmb/6NNZQHeEhR-wNWA8pZrm1MXtrAs=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/clouds-5b6b4e50c9e77c0050491212.jpg"/>
-          </Col>
+          {/* <Col className="second-col p-0">            
+            <div className="bg h-100"></div>
+          </Col> */}
         </Row>
       </Container>
     </div>
--- src/presentation/components/SignUp.tsx
+++ src/presentation/components/SignUp.tsx
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from "react";
 import SignUpUsecase from "../../domain/signup/SignUpUsecase";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 type SignUpProps = {
 
 }
 
 const SignUp = (signUpProps:SignUpProps)=>{
-
     return (
-        <div style={{margin:'auto', width:500}}>
-            <form>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>username</td>
-                            <td><input type="text" /></td>
-                        </tr>
-                        <tr>
-                            <td>password</td>
-                            <td><input type="password" /></td>
-                        </tr>
-                        <tr>
-                            <td>re enter password</td>
-                            <td><input type="password" /></td>
-                        </tr>
-                    </tbody>                    
-                </table>          
-                <input type="button" value="Sign Up" />                  
-            </form>            
-            {/* <Router> */}
-                <Link to="/">Login</Link>
-            {/* </Router> */}
-
+        <div className="login-wrapper">
+            <Form>
+                <Form.Group>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Re enter password</Form.Label>
+                    <Form.Control type="password" />
+                </Form.Group>                
+                <Container className="">
+                    <Row className="">
+                        <Col className="p-0"><Form.Label className="col-form-label-sm"><Link to="/">Login</Link></Form.Label></Col>
+                        <Col className="p-0 col-forgot-password"><Form.Label className="col-form-label-sm forgot-password">Forgot password?</Form.Label></Col>
+                    </Row>
+                </Container>
+                <div className="d-grid gap-2 mt-3">
+                    <Button variant="flat">Sign Up</Button>
+                </div>
+            </Form>
         </div>
     );
 }
