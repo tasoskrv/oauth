d1ea89bb94baf8ee20fec4ac0b53b4bfedc1cb0d
tasoskrv <tasos.karavanis@gmail.com>
2022-11-30 19:55:04 +0200

    Update RequestApis.tsx

--- src/infrastructure/apis/RequestApis.tsx
+++ src/infrastructure/apis/RequestApis.tsx
@@ -4,4 +4,4 @@ export default axios.create({
     // baseURL: 'http://192.168.1.119:8080/oauth/server/index.php'
     //baseURL: 'http://172.17.30.46:8080/oauth/server/index.php'
     baseURL:'http://127.0.0.1:8080/oauth/server/index.php/'
-});
+});  
